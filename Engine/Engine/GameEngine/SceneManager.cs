a0595d0c7847b79321c69abcb9d09152af3e098c
HorrellTech <horrelltechnologies@gmail.com>
2018-10-29 12:50:29 +1300

    Add scene manager

---
+++ Engine/Engine/GameEngine/SceneManager.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * The Scene Manager will handle all our scenes. These scenes will contain game objects and instances of those objects
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.GameEngine
+{
+    class SceneManager
+    {
+    }
+}
