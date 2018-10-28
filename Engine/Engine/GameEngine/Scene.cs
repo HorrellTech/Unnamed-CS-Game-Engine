d0c2636e3a5aeeb44307d12c4fdf53fa187ef7cd
HorrellTech <horrelltechnologies@gmail.com>
2018-10-29 12:52:11 +1300

    Add Scene.cs

---
+++ Engine/Engine/GameEngine/Scene.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * The Scene class will keep track of the Game Objects contained in it, with cameras/views, dimentions
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.GameEngine
+{
+    class Scene
+    {
+    }
+}
