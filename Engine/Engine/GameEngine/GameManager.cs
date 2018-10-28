8c521e6aa495aa2083995fe1fc66b1c06a3dc3e7
HorrellTech <horrelltechnologies@gmail.com>
2018-10-29 12:48:52 +1300

    Add GameManager.cs

---
+++ Engine/Engine/GameEngine/GameManager.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * The main class for managing our scenes, game play, resource loading etc
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.GameEngine
+{
+    class GameManager
+    {
+    }
+}
