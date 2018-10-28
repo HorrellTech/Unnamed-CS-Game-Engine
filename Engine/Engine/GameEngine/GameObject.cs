e118e46435dc8bd3b0ed5bbf7c0ff45c6edf1542
HorrellTech <horrelltechnologies@gmail.com>
2018-10-29 12:53:53 +1300

    Add GameObject.cs

---
+++ Engine/Engine/GameEngine/GameObject.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * A base class for all Game Objects to inherit from. Will handle things like basic physics by default(with the option to turn these features off if 
+ * the user decides they want to use their own physics)
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.GameEngine
+{
+    class GameObject
+    {
+    }
+}
