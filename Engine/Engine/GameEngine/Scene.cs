350ecbd6a27bab2670f8dba7d19f58ad39e195a4
HorrellTech <horrelltechnologies@gmail.com>
2018-10-29 15:02:00 +1300

    Add loop methods

--- Engine/Engine/GameEngine/Scene.cs
+++ Engine/Engine/GameEngine/Scene.cs
@@ -14,12 +14,86 @@ namespace Engine.GameEngine
     {
         private List<GameObject> objects = new List<GameObject>(); // Hold a list of all the objects in the scene
 
+        // Variables
+        private float width; // The width of the scene in pixels
+        private float height; // The height of the scene in pixels
+        private float viewX; // The X position of the main view
+        private float viewY; // The Y position of the main view
+        private float viewW; // The width of the main view
+        private float viewH; // The height of the main view
+
         /// <summary>
         /// Constructor for the scene
         /// </summary>
         public Scene()
         {
 
+        }
+
+        /// <summary>
+        /// Main loop for the scene. Will handle the logic and drawing of the objects instances
+        /// </summary>
+        public void Loop()
+        {
+            // OBJECT UPDATES
+            if(objects.Count > 0)
+            {
+                // Itterate throught each object
+                foreach(var ob in objects)
+                {
+                    if (ob.GetInstances.Count > 0)
+                    {
+                        // Loop through the object instances
+                        foreach (var inst in ob.GetInstances)
+                        {
+                            if(inst.Active) // Make sure the instance is active
+                            {
+                                inst.Loop(); // Perform the event
+                            }
+                        }
+                    }
+                }
+            }
+
+            // OBJECT DRAWING
+            if (objects.Count > 0)
+            {
+                // Itterate throught each object
+                foreach (var ob in objects)
+                {
+                    if (ob.GetInstances.Count > 0)
+                    {
+                        // Loop through the object instances
+                        foreach (var inst in ob.GetInstances)
+                        {
+                            if (inst.Active) // Make sure the instance is active
+                            {
+                                inst.Draw(); // Perform the event
+                            }
+                        }
+                    }
+                }
+            }
+
+            // OBJECT DRAWING GUI
+            if (objects.Count > 0)
+            {
+                // Itterate throught each object
+                foreach (var ob in objects)
+                {
+                    if (ob.GetInstances.Count > 0)
+                    {
+                        // Loop through the object instances
+                        foreach (var inst in ob.GetInstances)
+                        {
+                            if (inst.Active) // Make sure the instance is active
+                            {
+                                inst.DrawGui(); // Perform the event
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
