e6a38fc18dde5b3a3d6c65505ca7594742fb94e0
HorrellTech <horrelltechnologies@gmail.com>
2018-10-30 23:34:26 +1300

    Add colors class so we don't have to use HEX to define a basic color

---
+++ Engine/Engine/GameEngine/Colors.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * A static class to return colors based on the color name rather than having to use HEX 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+
+namespace Engine.GameEngine
+{
+    public static class Colors
+    {
+        public static Color Black { get { return (Color.Black); } } // Black (duh)
+    }
+}
